f6cd89668856b563ef32882cc8095401b6e2742d
alexeyvolodin902 <alexeyvolodin902@gmail..com>
2016-11-01 16:35:05 +0300

    Добавлена модель слайдера

--- models/Slider.php
+++ models/Slider.php
@@ -5,3 +5,22 @@
  * Date: 01.11.16
  * Time: 16:02
  */
+
+class Slider
+{
+    //Возвращает массив с данными мастеров из бд
+    public static function getResourcesSlider()
+    {
+        $sliderList = array();
+        $db = Db::getConnection();
+        $result = $db->query("SELECT header, content ,url_image FROM indexSlider");
+        $i = 0;
+        while ($row = $result->fetch()) {
+            $sliderList[$i]['header'] = $row['header'];
+            $sliderList[$i]['content'] = $row['content'];
+            $sliderList[$i]['url_image'] = $row['url_image'];
+            $i++;
+        }
+        return $sliderList;
+    }
+}
