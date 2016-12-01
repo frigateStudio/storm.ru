bf9dbb280a1002fbd67a88a7cf3526c953d2a0f8
alexeyvolodin902 <alexeyvolodin902@gmail..com>
2016-12-01 17:16:48 +0300

    finished work on the validation form reviews
    Added functions js validation for all form fields connected third-party
    library validator.js

--- views/review.php
+++ views/review.php
@@ -79,10 +79,21 @@
 <script type="text/javascript" src="../template/slider/slick.min.js"></script>
 <script type="text/javascript" src="../template/js/validator.min.js"></script>
 <script>
-    var flErrorName = false; //общие ошибки
-    var flErrorEmail = false;
+
+    var errorMessages = ["",
+        "Пожалуйста, заполните это поле",
+        "Пожалуйста, введите корректный email адрес",
+        "Количество слов должно быть не более 3х",
+        "Ошибка ввода! Допускаются только русские символы"];
+
+    var codeErrorName = 0;
+    var codeErrorEmail = 0;
+    var errorEmptyName = false;
+    var errorEmptyEmail = false;
+    var errorEmptyReview = false;
     var errorName = $('#nameError');
     var inputName = document.getElementById('name');
+    var inputReview = document.getElementById('review');
     var inputEmail = $("#email");
     var errorEmail = $("#emailError");
 
@@ -93,6 +104,8 @@
         var container = $(".containerDoReview");
         var divSlider = $(".reviewSlider");
         var form = $(".formReview");
+
+
         //инициализация слайдера
         slider.show();
         slider.slick({
@@ -137,89 +150,142 @@
 
         //обработка измения значения поля Имя
         inputName.oninput = function () {
-         checkValidateName();
-
+            checkValidateName();
         };
-        //обработка изменений поля email после выхода из фокуса
-        inputEmail.change(function () {
-            flErrorEmail=false;
-            errorEmail.text("");
-            if (!validator.isEmail(inputEmail.val())) {
-                errorEmail.text("Введите корректный email адрес");
-                flErrorEmail = true;
-            }
-            else
-                flErrorEmail = false;
 
+        //обработка изменений review после tab
+        $("#review").change(function () {
+            checkEmptyInput("review");
         });
 
 
+        //обработка изменений поля email после выхода из фокуса
+        inputEmail.focusout(function () {
+            checkValidEmail();
+        });
+
+        //Проверка на пустоту поля review и выдача сообщения
+        inputReview.oninput = function () {
+            var input = $("#reviewError");
+            input.text(errorMessages[0]);
+            if (inputReview.value == "")
+                input.text(errorMessages[1]);
+        }
     });
 
 
     //проверка на пустоту всех полей формы и наличия остальных ошибок
     function checkForm() {
-
         checkEmptyInput("name");
         checkEmptyInput("email");
         checkEmptyInput("review");
 
-        return checkEmptyInput("name") && checkEmptyInput("email") && checkEmptyInput("review")
-            && checkValidEmail() && checkValidateName() && !flErrorEmail && !flErrorName;
+        if (errorEmptyReview)
+            $("#review").focus();
+        if (errorEmptyEmail)
+            inputEmail.focus();
+        if (errorEmptyName)
+            $("#name").focus();
 
+
+        if (!errorEmptyEmail) {
+            if (!checkValidEmail())
+                $("#email").focus();
+        }
+        if (!errorEmptyName) {
+            if (codeErrorName != 0) {
+                checkValidateName();
+                $("#name").focus();
+            }
+        }
+        return (!errorEmptyName && codeErrorName == 0 && !errorEmptyEmail && !errorEmptyReview && codeErrorEmail == 0);
     }
 
     //проверка на пустоту поля с именем nameInput
     //и выдача сообщения об ошибке в span #nameInputError
     function checkEmptyInput(nameInput) {
+        switch (nameInput) {
+            case "name":
+                errorEmptyName = false;
+                break;
+            case "email":
+                errorEmptyEmail = false;
+                break;
+            case "review":
+                errorEmptyReview = false;
+                break;
+            default:
+                break;
+        }
         var message = $("#" + nameInput + "Error");
         var input = $("#" + nameInput);
         message.text("");
         if (input.val() == "") {
-            message.text("Пожауйста, заполните это поле");
-            input.focus();
+            message.text(errorMessages[1]);
+            if (nameInput == "name")
+                errorEmptyName = true;
+            if (nameInput == "email")
+                errorEmptyEmail = true;
+            if (nameInput == "review")
+                errorEmptyReview = true;
             return false;
         }
-        else return true;
+        else {
+            return true;
+        }
     }
+    //проверка email на валидность и выдача сообщения об ошибке
     function checkValidEmail() {
+        codeErrorEmail = 0;
+        errorEmail.text(errorMessages[0]);
         if (!validator.isEmail($("#email").val())) {
-            $("#emailError").text("Введите корректный email адрес");
-
+            $("#emailError").text(errorMessages[2]);
+            codeErrorEmail = 1;
             return false;
-
         }
-        flErrorEmail=false;
         return true;
     }
+
+    //проверка на валидность имени
     function checkValidateName() {
-        errorName.text("");
-        flErrorName = false;
+        $("#name").focus();
+        errorName.text(errorMessages[0]);
+        codeErrorName = 0;
+        if (inputName.value == "") {
+            errorName.text(errorMessages[1]);
+            codeErrorName = 3;// поле не заполнено
+            return;
+        }
         var arrName = inputName.value.split(" ");
         if (arrName.length > 3) {
-            errorName.text("Коилчество слов должно быть не более 3х");
-            flErrorName=true;
+            errorName.text(errorMessages[3]);
+            codeErrorName = 1;//Больше 3х слов
             return;
         }
 
         for (var i = 0; i < arrName.length; i++) {
             if (arrName[i] == "")
                 break;
-            else if (!validator.isAlpha(arrName[i], 'ru-RU')) {
-                errorName.text("Ошибка ввода! Допускаются только русские символы");
-                flErrorName = true;
+            if (!validator.isAlpha(arrName[i], 'ru-RU')) {
+                errorName.text(errorMessages[4]);
+                codeErrorName = 2;//Неверный язык
                 break;
+
             }
             else
-                flErrorName = false;
+                codeErrorName = 0;
         }
         if (inputName.value == "") {
-            errorName.text("");
-            flErrorName = true;
+            errorName.text(errorMessages[0]);
+            errorEmptyName = true;
+
         }
-        return true;
+
     }
+
     //------конец валидации------
+
+
     //------отправление формы ajax-------
     function call() {
 
@@ -235,7 +301,7 @@
                     $("#recordReview").hide();
                     setTimeout(function () {
                         $("#closeForm").click();
-                    }, 1000);
+                    }, 1500);
                     $("#name").val('');
                     $("#email").val('');
                     $("#review").val('');
