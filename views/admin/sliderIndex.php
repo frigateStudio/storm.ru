2c40e8a9c72f775622810f9fca4539e20670cc6c
alexeyvolodin902 <alexeyvolodin902@gmail..com>
2016-12-04 17:39:49 +0300

    add form "add slide"
    no work

--- views/admin/sliderIndex.php
+++ views/admin/sliderIndex.php
@@ -23,11 +23,11 @@ include ROOT . '/views/layouts/adminHeader.php';
                 <p><?php echo $slide['content']; ?></p>
                 <img src="<?php echo $slide['url_image'] ?>"><br>
 
-                <a  href="#" class="editButton">Изменить</a>
-                <?php if (count($slides)>1):?>
+                <a href="#" class="editButton">Изменить</a>
+                <?php if (count($slides) > 1): ?>
 
-                <a href="deleteSlide/<?php echo $slide['id']; ?>" class="editButton">Удалить</a>
-                <?php endif;?>
+                    <a href="deleteSlide/<?php echo $slide['id']; ?>" class="editButton">Удалить</a>
+                <?php endif; ?>
             </div>
 
             <?php
@@ -38,6 +38,26 @@ include ROOT . '/views/layouts/adminHeader.php';
     <?php if (count($slides) < 10)
         echo '<a href="#" class="addSlide">Добавить слайд</a>';
     ?>
+
+
+    <div class="formAddSlide">
+
+        <form enctype="multipart/form-data" action="/" method="POST">
+            <p>
+                <label for="head">Заголовок</label><br>
+                <input type="text" id="head" name="head">
+            </p>
+            <p>
+                <label for="desc">Текст</label><br>
+                <textarea type="text" id="desc" name="desc"></textarea>
+            </p>
+            <input type="hidden" name="MAX_FILE_SIZE" value="30000"/>
+            <label for="imgSlide">Изображение</label><br>
+            <input name="imgSlide"  type="file" accept="image/*"/><br>
+            <button id="buttonAdd">Добавить</button>
+        </form>
+
+    </div>
 
 
 </main>
